f04364131fa5aa05d56600e8aa5225e486592dcf
Carolina Lopes <carolcandela006@gmail.com>
2026-04-06 21:01:52 -0300

    atividade api

--- Api/Index.js
+++ Api/Index.js
@@ -1,23 +1,51 @@
-// config inicial chamar o express vai procurar o módulo
 const express = require('express')
-const app = express() // Inicializar as apps 
+const app = express()
+const port = 3000
+const mongoose = require('mongoose')
+const Person = require('./models/Person')
 
-//forma de ler JSON UTILIZAR MIDDLEWARES 
-app.use( //criando  o MIDDLEWARES
-    express.urlencoded({
-      extended: true,
-    }),
-  )
 
-  app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
 
-  //rota inicial GET pegar algo so servidor  endpoint 
-  app.get('/',  (req, res) => {
+app.use(express.json())
 
-  //mostrar requisição mostrar a resposta que vai ser JSON
-    res.json({ message: 'Oi Express'})
+app.post('/person', async (req, res) => {
+  const { name, salary, approved } = req.body
+
+  const person = {
+    name,
+    salary,
+    approved,
+  }
+
+  try {
+    await Person.create(person)
+
+    res.status(201).json({ message: 'Pessoa inserida no sistema com sucesso!' })
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
 })
-//23 minutos 12 
-  //entregar a porta
-  app.listen(3000)
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Olá❤️' })
+})
+
+const DB_USER = 'carolina'
+const DB_PASSWORD = encodeURIComponent('senha')
+
+mongoose
+.connect(
+    `mongodb+srv://${DB_USER}${DB_PASSWORD}carolina:<db_password>@cluster0.sutn45z.mongodb.net/?appName=Cluster0`,
+)
+ .then(() => {
+    console.log('Conectou ao banco!')
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
 
